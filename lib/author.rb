6fabf043ebd736eed40972dbe0ae63659c32783b
Johannes Hupfeld <j.hupfeld@gmail.com>
2020-01-27 21:30:42 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,29 +1,24 @@
-class Author 
-   attr_accessor :name
-   @@post_count = 0
+class Author
+  attr_accessor :name
 
-    def initialize(name)
-     @name = name
-     @posts = [ ]
-   end
+  def initialize(name)
+    @name = name
+  end
 
-    def posts
-     @posts
-   end
+  def posts
+    Post.all.select { |post| post.author == self }
+  end
 
-    def add_post(post)
-     @posts << post
-     post.author = self
-     @@post_count += 1
-   end
+  def add_post(post)
+    post.author = self
+  end
 
-    def add_post_by_title(post_title)
-     post = Post.new(post_title)
-     @posts << post
-     post.author = self
-     @@post_count += 1
-   end
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
 
-    def self.post_count
-     @@post_count
-   end
+  def self.post_count
+    Post.all.count
+  end
+end
