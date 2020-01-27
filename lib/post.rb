2b592bdd9e2c9b1ab0adc8bea948b1ec574f2ae9
Johannes Hupfeld <j.hupfeld@gmail.com>
2020-01-27 21:30:59 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,14 +1,21 @@
 class Post
-   attr_accessor :author, :title 
+  attr_accessor :title, :author
 
-    def initialize(title)
-     @title = title
-   end
+  @@all = []
 
-    def author_name
-     if author 
-       self.author.name 
-     else
-      
-     end
-   end
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+    author&.name
+    # same as
+    # author.name if author
+  end
+
+end
